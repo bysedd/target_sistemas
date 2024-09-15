27d4bcff00946758aa208699e7ae26a71862a28f
Felippe <4drade@gmail.com>
2024-09-15 02:42:33 -0300

    refactor: Simplify calculation of invoice representation percentage
    Simplify the calculation of the invoice representation percentage by renaming the `Data` type alias to `Invoices` and updating the function signatures and variable names accordingly. This improves code readability and maintainability.

--- ex03.py
+++ ex03.py
@@ -15,46 +15,45 @@ from typing import TypeAlias
 
 from icecream import ic
 
-Number: TypeAlias = int | float
-Data: TypeAlias = list[dict[str, Number]]
+Invoices: TypeAlias = list[dict[str, int | float]]
 
 
-def load_data() -> Data:
+def load_data() -> Invoices:
     """
     Carrega dados de um arquivo JSON localizado no diretório 'data' relativo à localização do script.
 
     Returns:
-        Data: Os dados carregados a partir do arquivo “dados.json”.
+        Invoices: Os dados carregados a partir do arquivo “dados.json”.
     """
     data_dir = Path(__file__).parent / "data"
     with open(data_dir / "dados.json") as f:
-        data: Data = json.load(f)
+        data: Invoices = json.load(f)
     return data
 
 
-def calculate_statistics(invoices: Data) -> dict[str, Number]:
+def calculate_statistics(invoices: Invoices) -> dict[str, float]:
     """
     Calcula várias estatísticas a partir de uma lista de faturamentos.
 
     Args:
-        invoices (Data): Uma lista de dicionários em que cada dicionário representa uma fatura
+        invoices (Invoices): Uma lista de dicionários em que cada dicionário representa uma fatura
                          e contém uma chave de “valor” com um valor numérico.
 
     Returns:
-        dict[str, Number]: Um dicionário que contém as seguintes estatísticas:
+        (dict[str, float]): Um dicionário que contém as seguintes estatísticas:
             - "menor_faturamento": O menor valor da fatura.
             - "maior_faturamento": O maior valor da fatura.
             - "dias_acima_media": O número de dias com valores de fatura acima da média mensal.
     """
     invoice_values = [f["valor"] for f in invoices if f["valor"] > 0]
-    media_mensal = sum(invoice_values) / len(invoice_values)
-    dias_acima_media = sum(1 for f in invoice_values if f > media_mensal)
-    menor_faturamento = min(invoice_values)
-    maior_faturamento = max(invoice_values)
+    monthly_average = sum(invoice_values) / len(invoice_values)
+    days_above_average = sum(1 for f in invoice_values if f > monthly_average)
+    minimum_invoice = min(invoice_values)
+    maximum_invoice = max(invoice_values)
     return {
-        "menor_faturamento": menor_faturamento,
-        "maior_faturamento": maior_faturamento,
-        "dias_acima_media": dias_acima_media,
+        "menor_faturamento": minimum_invoice,
+        "maior_faturamento": maximum_invoice,
+        "dias_acima_media": days_above_average,
     }
 
 
